ea1c187ed7ab0184a92b6dd3ee1ac78a454ff6d4
Xados <jxds.lph@pm.me>
2022-12-06 12:57:56 +0100

    Update stats.py

--- src/game/gameplay/stats.py
+++ src/game/gameplay/stats.py
@@ -2,7 +2,6 @@ class Stats():
 	def __init__(self, opt={}):
 		self.speed = 300
 		self.jumpForce = 12
-		self.jumpForce = 13
 		self.jumpMax = 1
 		self.glide = 0 #en secondes
 		self.dash = 0 #en frame
